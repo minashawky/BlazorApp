9191767d904cf7d0006c64b9025669fb172021d3
Mina Shawky <mina.shawky@live.com>
2020-07-11 21:39:44 +0400

    Creating more Razor components and custom CSS

---
+++ BlazorApp/Shared/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public bool InTheatres { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string TitleBrief { get { return Title.Substring(0, 15); } }
+    }
+}
---
+++ BlazorApp/Shared/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
