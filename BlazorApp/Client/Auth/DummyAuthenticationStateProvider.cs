21ed033f1868c3f82d9abc03017a8cc47f84e4d6
Mina Shawky <mina.shawky@live.com>
2020-07-28 08:59:02 +0400

    Adding Claims based authentication

---
+++ BlazorApp/Client/Auth/DummyAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Auth
+{
+    public class DummyAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymous = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Mina"),
+                new Claim(ClaimTypes.Role, "Admin")
+            });
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
--- BlazorApp/Client/Program.cs
+++ BlazorApp/Client/Program.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Blazor.FileReader;
+using Tewr.Blazor.FileReader;
 using BlazorApp.Client.Helpers;
 using BlazorApp.Client.Repository;
 using BlazorApp.Shared;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorApp.Client.Auth;
 
 namespace BlazorApp.Client
 {
@@ -27,6 +29,8 @@ namespace BlazorApp.Client
             builder.Services.AddScoped<IGenreRepository, GenreRepository>();
             builder.Services.AddScoped<IPersonRepository, PersonRepository>();
             builder.Services.AddScoped<IMoviesRepository, MoviesRepository>();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, DummyAuthenticationStateProvider>();
 
             await builder.Build().RunAsync();
         }
